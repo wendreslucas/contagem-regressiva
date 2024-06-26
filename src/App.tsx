ee6ca1b7440ded14c7e6428655b31d28ecf2bf61
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2024-06-26 00:26:34 -0300

    Adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HamsterWheel from "./components/HamsterWheel";
 import CountdownTimer from "./components/Clock";
+import BackgroundScene from "./components/BackgroundScene";
 
 const App: React.FC = () => {
   return (
     <main className="relative min-h-screen bg-gray-900 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full flex flex-col justify-center items-center">
+      <BackgroundScene />
+      <div className="z-10 max-w-7xl w-full flex flex-col justify-center items-center">
         <CountdownTimer />
         <HamsterWheel />
       </div>
