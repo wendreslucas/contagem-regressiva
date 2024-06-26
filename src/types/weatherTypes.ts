267be855c87a8c027e456d8aa5922ae5ac32c61b
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2024-06-26 17:44:37 -0300

    svgs

--- src/types/weatherTypes.ts
+++ src/types/weatherTypes.ts
@@ -6,9 +6,9 @@ export type WeatherInfoIconTypes =
   | "pressure";
 
 export const WeatherInfoIcons: Record<WeatherInfoIconTypes, string> = {
-  sunset: "../../public/icons/temp.svg",
-  sunrise: "../../public/icons/temp.svg",
-  humidity: "../../public/icons/humidity.svg",
-  wind: "../../public/icons/wind.svg",
-  pressure: "../../public/icons/pressure.svg",
+  sunset: "/icons/temp.svg",
+  sunrise: "/icons/temp.svg",
+  humidity: "/icons/humidity.svg",
+  wind: "/icons/wind.svg",
+  pressure: "/icons/pressure.svg",
 };
